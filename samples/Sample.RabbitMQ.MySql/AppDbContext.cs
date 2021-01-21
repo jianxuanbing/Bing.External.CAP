6439215084569afbae974ad456f3ffd32c1542a8
jian玄冰 <jianxuanhuo1@126.com>
2021-01-21 21:36:49 +0800

    sample: 新增CAP例子

---
+++ samples/Sample.RabbitMQ.MySql/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sample.RabbitMQ.MySql
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}, Id:{Id}";
+        }
+    }
+    public class Person2
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}, Id:{Id}";
+        }
+    }
+    public class AppDbContext : DbContext
+    {
+        public const string ConnectionString = "Server=10.186.132.21;Database=bing_admin;UserId=root;Password=bing2019.00;";
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(ConnectionString);
+        }
+    }
+}
---
+++ samples/Sample.RabbitMQ.MySql/Startup.cs
@@ -0,0 +1,45 @@
+﻿using DotNetCore.CAP.Messages;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Sample.RabbitMQ.MySql
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>();
+
+            services.AddCap(x =>
+            {
+                x.UseEntityFramework<AppDbContext>();
+                x.UseRabbitMQ(o =>
+                {
+                    o.UserName = "admin";
+                    o.Password = "bing2019.00";
+                    o.HostName = "10.186.132.60";
+                });
+                x.UseDashboard();
+                x.FailedRetryCount = 5;
+                x.FailedThresholdCallback = failed =>
+                {
+                    var logger = failed.ServiceProvider.GetService<ILogger<Startup>>();
+                    logger.LogError($@"A message of type {failed.MessageType} failed after executing {x.FailedRetryCount} several times, 
+                        requiring manual troubleshooting. Message name: {failed.Message.GetName()}");
+                };
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
